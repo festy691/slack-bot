f68732c6aed7fe6d884b7d6721037898e6791aa6
festus francis <festy691@gmail.com>
2021-11-13 10:08:13 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+import { App } from '@slack/bolt';
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.post('/welcome', (req,res)=>{
+    console.log(req.body.text);
+    return res.status(200).send('Welcome. How are you doing?');
+});
+// Starts server
+app.listen(port, function() {
+    console.log('Bot is listening on port ' + port)
+})
