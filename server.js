20504a71ec70d1941ab0ed1ebc20bd7b9af8723b
festus francis <festy691@gmail.com>
2021-11-19 07:56:14 +0100

    return response as json

--- server.js
+++ server.js
@@ -8,9 +8,44 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+
 app.post('/welcome', (req,res)=>{
+    let message = {
+        "text": "how are you doing?",
+        "response_type": "in_channel",
+        "attachments": [
+            {
+                "text": "How are you doing?",
+                "fallback": "How are you doing?",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "callback_id": "game_selection",
+                "actions": [
+                    {
+                        "name": "greeting",
+                        "text": "Pick a response...",
+                        "type": "select",
+                        "options": [
+                            {
+                                "text": "Doing well",
+                                "value": "well"
+                            },
+                            {
+                                "text": "Neutral",
+                                "value": "neutral"
+                            },
+                            {
+                                "text": "Feeling lucky",
+                                "value": "lucky"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
     console.log(req.body.text);
-    return res.status(200).send('Welcome. How are you doing?');
+    return res.status(200).json(message);
 });
 // Starts server
 app.listen(port, function() {
